d7929b73b577f265fb7f45994214ed892549c39f
denis <waridesu@gmail.com>
2023-08-22 18:32:57 +0300

    adding dto

--- src/post/dto/post.dto.ts
+++ src/post/dto/post.dto.ts
@@ -5,30 +5,37 @@ import {
   IsOptional,
   IsString,
 } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class PostDto {
   @IsNotEmpty()
   @IsString()
+  @ApiProperty()
   readonly title: string;
 
   @IsNotEmpty()
   @IsString()
+  @ApiProperty()
   readonly content: string;
 
   @IsOptional()
   @IsString()
+  @ApiProperty()
   readonly author?: string;
 
   @IsOptional()
   @IsArray()
   @IsString({ each: true })
+  @ApiProperty()
   readonly categories?: string[];
 
   @IsOptional()
   @IsDate()
+  @ApiProperty()
   readonly createdDate?: Date;
 
   @IsOptional()
   @IsString()
+  @ApiProperty()
   readonly imageUrl?: string;
 }
