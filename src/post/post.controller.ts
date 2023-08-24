cc553a642586cdef4f640269173089857d35b443
denis <waridesu@gmail.com>
2023-08-24 13:20:22 +0300

    adding dto

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,15 +1,16 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { PostService } from './post.service';
-import { ApiBody, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { PostDto } from './dto/post.dto';
 
 @ApiTags('posts')
-@Controller('posts')
+@Controller()
 export class PostController {
   constructor(private readonly postService: PostService) {}
 
   @Post()
   @ApiOperation({ summary: 'Create a post' })
+  @ApiResponse({ status: 201, description: 'Created.' })
   @ApiBody({ type: PostDto })
   create(@Body() data: any) {
     return this.postService.create(data);
