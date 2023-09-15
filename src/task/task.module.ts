abba26ae8f14d5ebc0117bad3374c348b9ec59ba
Vladislav Virchenko <virchenko.vlad.2011@icloud.com>
2023-09-15 08:24:30 +0300

    Update task.module.ts

--- src/task/task.module.ts
+++ src/task/task.module.ts
@@ -4,9 +4,10 @@ import { TaskController } from './task.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/users/entities/user.entity';
 import { UsersService } from 'src/users/users.service';
+import { Book } from 'src/users/entities/book.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User, Book])],
   controllers: [TaskController],
   providers: [TaskService, UsersService],
 })
