4499ce03dee1365ad9151a4d2896c53678bd965a
Vladislav Virchenko <virchenko.vlad.2011@icloud.com>
2023-09-27 15:23:50 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: 'https://localhost:5173',
     methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE'],
     allowedHeaders: ['*'],
     preflightContinue: false,
