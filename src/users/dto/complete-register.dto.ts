e86c0b4fd3570245c45802d7c73d6d427cdba6ab
Morgan <otovmid@gmail.com>
2023-10-03 19:20:39 +0300

    fix/develop(added password checker)

--- src/users/dto/complete-register.dto.ts
+++ src/users/dto/complete-register.dto.ts
@@ -1,6 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
 
-import { IsNotEmpty, MinLength } from 'class-validator';
+import { IsNotEmpty, Matches, MinLength } from 'class-validator';
 
 export class UpdateUserRegisterDto {
   @IsNotEmpty()
@@ -17,10 +17,13 @@ export class UpdateUserRegisterDto {
   nickName: string;
 
   @IsNotEmpty()
-  @ApiProperty({ example: 'string' })
-  @MinLength(6)
+  @Matches(/^(?=.*\d)(?=.*[A-Z])[A-Za-z\d]+$/, {
+    message: 'Password must contain min 1 digit, min 1 uppercase letter and only Latin alphabet.'
+   })
+  @ApiProperty({example: 'string'})
+  @MinLength(8)
   password: string;
-
+  
   @IsNotEmpty()
   @ApiProperty()
   @MinLength(6)
