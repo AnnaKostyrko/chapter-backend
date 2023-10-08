07e37206ade8b43d84951ae54025bc67f5f77e13
denis <waridesu@gmail.com>
2023-10-08 14:03:41 +0300

    update comment:
    - swagger fixes

--- src/comment/comment.controller.ts
+++ src/comment/comment.controller.ts
@@ -1,14 +1,17 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { CommentService } from './comment.service';
 import { CommentEntity } from './entity/comment.entity';
 import { CreateCommentDto } from './dto/comment.dto';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { AuthGuard } from '@nestjs/passport';
 
 @ApiTags('Comment')
 @Controller('comment')
 export class CommentController {
   constructor(private readonly commentService: CommentService) {}
 
+  @ApiBearerAuth()
+  @UseGuards(AuthGuard('jwt'))
   @Post()
   async create(@Body() commentData: CreateCommentDto): Promise<CommentEntity> {
     return await this.commentService.create(commentData);
