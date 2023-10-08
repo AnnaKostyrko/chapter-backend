6e33432a359ac9b06331294ce0ba505c02ba246b
denis <waridesu@gmail.com>
2023-10-08 13:59:45 +0300

    update comment:
    - swagger fixes

--- src/comment/comment.controller.ts
+++ src/comment/comment.controller.ts
@@ -2,7 +2,9 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { CommentService } from './comment.service';
 import { CommentEntity } from './entity/comment.entity';
 import { CreateCommentDto } from './dto/comment.dto';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Comment')
 @Controller('comment')
 export class CommentController {
   constructor(private readonly commentService: CommentService) {}
--- src/comment/comment.module.ts
+++ src/comment/comment.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CommentEntity } from './entity/comment.entity';
+import { CommentController } from './comment.controller';
+import { CommentService } from './comment.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([CommentEntity])],
+  controllers: [CommentController],
+  providers: [CommentService],
 })
 export class CommentModule {}
