d89b10f8f99d0e34c04a01c315a270337fe601da
denis <waridesu@gmail.com>
2023-10-09 19:00:30 +0300

    - code review

--- src/comment/comment.controller.ts
+++ src/comment/comment.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Param, Post, UseGuards } from '@nestjs/common';
 import { CommentService } from './comment.service';
 import { CommentEntity } from './entity/comment.entity';
 import { CreateCommentDto } from './dto/comment.dto';
@@ -12,8 +12,12 @@ export class CommentController {
 
   @ApiBearerAuth()
   @UseGuards(AuthGuard('jwt'))
-  @Post()
-  async create(@Body() commentData: CreateCommentDto): Promise<CommentEntity> {
+  @Post(':postId')
+  async create(
+    @Param('postId') postId: number,
+    @Body() commentData: CreateCommentDto,
+  ): Promise<CommentEntity> {
+    commentData.postId = postId;
     return await this.commentService.create(commentData);
   }
 }
--- src/comment/dto/comment.dto.ts
+++ src/comment/dto/comment.dto.ts
@@ -1,18 +1,44 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, Length } from 'class-validator';
+import { IsNotEmpty, Length, IsInt, IsOptional, Min } from 'class-validator';
 
 export class CreateCommentDto {
-  @ApiProperty()
+  @ApiProperty({
+    description: 'ID of the parent comment',
+    example: 1,
+    required: false,
+  })
+  @IsOptional()
+  @IsInt({ message: 'parentId must be an integer' })
+  @Min(1, { message: 'parentId must be greater than 0' })
   parentId?: number;
 
-  @ApiProperty()
-  @IsNotEmpty()
-  @Length(1, 255)
+  @ApiProperty({
+    description: 'Text of the comment',
+    example: 'Great post!',
+    minLength: 1,
+    maxLength: 255,
+  })
+  @IsNotEmpty({ message: 'Comment text should not be empty' })
+  @Length(1, 255, {
+    message: 'Comment text should be between 1 and 255 characters',
+  })
   text: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    description: 'ID of the post to which the comment belongs',
+    example: 10,
+  })
+  @IsNotEmpty({ message: 'postId is required' })
+  @IsInt({ message: 'postId must be an integer' })
+  @Min(1, { message: 'postId must be greater than 0' })
   postId: number;
 
-  @ApiProperty()
+  @ApiProperty({
+    description: 'ID of the user who left the comment',
+    example: 5,
+  })
+  @IsNotEmpty({ message: 'userId is required' })
+  @IsInt({ message: 'userId must be an integer' })
+  @Min(1, { message: 'userId must be greater than 0' })
   userId: number;
 }
