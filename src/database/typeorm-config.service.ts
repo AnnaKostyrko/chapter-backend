2d01460deca0375c075ef41c62db1c4403347500
Vladislav Virchenko <virchenko.vlad.2011@icloud.com>
2023-10-25 09:41:37 +0300

    Update typeorm-config.service.ts

--- src/database/typeorm-config.service.ts
+++ src/database/typeorm-config.service.ts
@@ -21,7 +21,7 @@ export class TypeOrmConfigService implements TypeOrmOptionsFactory {
       }),
       dropSchema: false,
       keepConnectionAlive: true,
-      // migrationsRun: true,
+      migrationsRun: true,
       logging:
         this.configService.get('app.nodeEnv', { infer: true }) !== 'production',
       entities: [__dirname + '/../**/*.entity{.ts,.js}'],
