684cece2d450838fde895e8f95cb1e3cb2465102
Vladislav Virchenko <virchenko.vlad.2011@icloud.com>
2023-12-05 17:32:24 +0200

    fix

--- src/post/dto/post.dto.ts
+++ src/post/dto/post.dto.ts
@@ -1,15 +1,18 @@
-import { IsOptional } from 'class-validator';
+import { IsOptional, IsString } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class PostDto {
+  @IsString()
   @IsOptional()
   @ApiProperty()
   readonly imageUrl?: string;
 
+  @IsString()
   @IsOptional()
   @ApiProperty()
   readonly caption?: string;
 
+  @IsString()
   @IsOptional()
   @ApiProperty()
   readonly title?: string;
--- src/post/dto/updatePost.dto.ts
+++ src/post/dto/updatePost.dto.ts
@@ -1,16 +1,19 @@
 import { ApiProperty } from '@nestjs/swagger';
 
-import { IsOptional } from 'class-validator';
+import { IsOptional, IsString } from 'class-validator';
 
 export class UpdatePostDto {
+  @IsString()
   @IsOptional()
   @ApiProperty()
   readonly imageUrl?: string;
 
+  @IsString()
   @IsOptional()
   @ApiProperty()
   readonly caption?: string;
 
+  @IsString()
   @IsOptional()
   @ApiProperty()
   readonly title?: string;
