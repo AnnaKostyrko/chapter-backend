f17fb47b078941ac30ca2d396948958afccf0bb3
Vladislav Virchenko <virchenko.vlad.2011@icloud.com>
2023-12-28 18:19:14 +0200

    Update check.hash.validity.ts

--- src/utils/validators/check.hash.validity.ts
+++ src/utils/validators/check.hash.validity.ts
@@ -4,7 +4,7 @@ export const checkHashValidity = (hashDate: Date): void => {
   const currentDate = new Date();
   const timeDifference = (currentDate.getTime() - hashDate.getTime()) / 60000;
 
-  if (timeDifference >= 135) {
+  if (timeDifference >= 15) {
     throw new HttpException(
       {
         status: HttpStatus.BAD_REQUEST,
