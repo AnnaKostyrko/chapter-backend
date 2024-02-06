12aa7637501460b18ff8dcca0b4a01adac17f42d
Vladislav Virchenko <virchenko.vlad.2011@icloud.com>
2024-02-06 13:03:03 +0200

    Update book.regexp.ts

--- src/helpers/regex/book.regexp.ts
+++ src/helpers/regex/book.regexp.ts
@@ -1,2 +1,3 @@
-export const bookRegExp = /^[a-zA-Zа-яА-ЯіІїЇєЄґҐ\s]+$/;
+export const bookRegExp =
+  /^[a-zA-Zа-яА-ЯіІїЇєЄґҐ\s!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]+$/;
 export const bookMessage = 'Field must contain only Cyrillic or Latin letters';
